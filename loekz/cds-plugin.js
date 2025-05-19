adbb93d41bd3cfa6158f1692790754662d89a599
Stergios Marmaras <stergios.marmaras@mhp.com>
2025-05-19 18:57:02 +0000

    enable delete hidden on button when element is annotated

--- loekz/cds-plugin.js
+++ loekz/cds-plugin.js
@@ -1,33 +1,30 @@
 
 const cds = require('@sap/cds');
 const log = cds.log("LOEKZ Plugin");
-const {
-    setFlagTrue
-} = require('./deleteHandler');
 
 log.debug("is starting....")
 
-//check if Entity is annotated
+//check if Entity is annotated and not inside a Union
 const isAnnotated = (entity) => (
     (entity['@loekz']
         || entity.elements && Object.values(entity.elements).some(e => e['@loekz'])) && entity.query?.SET?.op !== 'union'
 )
 
-cds.once('served', () => {
+cds.once('served', () => {  //Services und Entitäten müssen ERST geladen werden, bevor sie verarbeitet werden können
 
-    for (const srv of cds.services) {
+    for (const srv of cds.services) {  // schau in den Services welche Entitäten aus den Services annotiert sind
         for (const entity of Object.values(srv.entities)) {
             if (isAnnotated(entity)) {
 
-                srv.before("DELETE", entity, setFlagTrue)
+                ///srv.after("DELETE", entity, setFlagTrue)
+                entity['@UI.DeleteHidden'] = true;
+
+
             }
         }
     }
 
 });
-
-
-
 
 
 
